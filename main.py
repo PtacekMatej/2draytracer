dcdc3c2312a46a14b23be934c12ff2bd565450d7
Matej Ptacek <matej.ptacek@oddin.gg>
2024-05-17 10:47:39 +0200

    draw minimap

--- main.py
+++ main.py
@@ -1,4 +1,25 @@
 import pygame as pg
+from math import *
+
+
+
+BLOCK_SIZE = 16
+GRID_SIZE = 8
+
+GAME_WORLD = [
+    1, 1, 1, 1, 1, 1, 1, 1,
+    1, 0, 0, 0, 0, 0, 0, 1, 
+    1, 0, 1, 0, 0, 1, 0, 1, 
+    1, 0, 0, 0, 0, 0, 0, 1, 
+    1, 0, 0, 1, 0, 0, 0, 1, 
+    1, 0, 1, 1, 0, 1, 0, 1, 
+    1, 0, 0, 0, 0, 1, 0, 1, 
+    1, 1, 1, 1, 1, 1, 1, 1
+]
+
+
+
+
 
 
 def main():
@@ -10,9 +31,17 @@ def main():
 
 
     while True:
+
+        for block_idx in range(len(GAME_WORLD)):
+            if GAME_WORLD[block_idx] == 1:
+                pg.draw.rect(screen, (255, 255, 255), pg.Rect(BLOCK_SIZE * (block_idx % 8), BLOCK_SIZE * (block_idx // 8), BLOCK_SIZE, BLOCK_SIZE))
+
+
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 pg.quit()
+
+        pg.display.flip()
 
 
 main()
