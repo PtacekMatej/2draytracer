0c5846459c485e60d6d369d2876b41ed3fae2b27
Matej Ptacek <matej.ptacek@oddin.gg>
2024-05-18 20:57:11 +0200

    generate different world

--- worldBuilder.py
+++ worldBuilder.py
@@ -6,7 +6,7 @@ def build(size):
         world[size * i] = 1
         world[size * i - 1] = 1
 
-    for i in filter(lambda x: x % 14 == 0, range(size*size)):
+    for i in filter(lambda x: ((x % 16 == 0 or (x // size) % 16 == 0) and not (x % 16 == 8 or (x // size) % 16 == 8)) or (x % 16 == 8 and (x // size) % 16 == 8) , range(size*size)):
         world[i] = 1
     
     return world
