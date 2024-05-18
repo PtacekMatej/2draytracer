1e94dd8eea86d301bd0c29c9bdebf56825c7d8e0
Matej Ptacek <matej.ptacek@oddin.gg>
2024-05-18 14:05:20 +0200

    visualize fps

--- main.py
+++ main.py
@@ -5,33 +5,41 @@ from renderer import render
 from rayTracer import traceRay
 
 
-
-
-
-
-
-
-
 def main():
     pg.init()
-    font = pg.font.Font('freesansbold.ttf', 32)
 
     screen = pg.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT), pg.RESIZABLE)
-    pg.display.set_caption('demo')
+    pg.display.set_caption('Ray Trecer 2D')
 
     clock = pg.time.Clock()
+    fps = pg.time.Clock()
 
     PLAYER_ANGLE = 2*PI/3
     PLAYER_POS = [128, 128]
+
+    font = pg.font.Font('freesansbold.ttf', 16)
+
+    cntr = 100
+    text = font.render(str(int(100000/fps.tick())) + " fps", True, (255, 255, 255))
+    textRect = text.get_rect()
 
 
     pg.mouse.set_visible(False)
 
     while True:
+        cntr = (1+cntr)%100
         handleEvents(screen)
         screen.fill((0, 0, 0))
         render(PLAYER_POS, PLAYER_ANGLE, screen)
         PLAYER_POS, PLAYER_ANGLE = handleMovement(PLAYER_POS, PLAYER_ANGLE, clock)
+
+
+    
+        if cntr == 0:
+            text = font.render(str(int(100000/fps.tick())) + " fps", True, (255, 255, 255))
+            textRect = text.get_rect()
+        screen.blit(text, textRect)
+
         pg.display.flip()
 
 
